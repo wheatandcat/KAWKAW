47a998b534a048064598377978bf4cd157ec8d24
wheatandcat <wheatandcat@gmail.com>
2026-02-22 12:20:25 +0900

    fix

--- server/db.ts
+++ server/db.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
-import * as schema from "@shared/schema";
+import * as schema from "../shared/schema";
 
 const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
--- server/routes.ts
+++ server/routes.ts
@@ -1,7 +1,7 @@
 import type { Express } from "express";
 import { createServer, type Server } from "http";
 import { storage } from "./storage";
-import { insertReviewSchema } from "@shared/schema";
+import { insertReviewSchema } from "../shared/schema";
 import { generateOgImage } from "./og-image";
 import { getProductById } from "./product-data";
 import { moderateText } from "./moderation";
